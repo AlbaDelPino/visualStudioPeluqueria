38d4a4b28a4eea2a6ab0e86ad44b36fe7e5b17cc
irecortas <ictjmb@gmail.com>
2026-01-15 10:39:45 +0100

    Reapply "fdf"
    This reverts commit fc6dd9a10d8bb0cf768d5cc4551bdb52a62f5606.

--- WinFormsApp1/WinFormsApp1/Principal.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Principal.Designer.cs
@@ -298,9 +298,10 @@
             labTituto.Location = new Point(15, 33);
             labTituto.Margin = new Padding(4, 0, 4, 0);
             labTituto.Name = "labTituto";
-            labTituto.Size = new Size(272, 39);
+            labTituto.Size = new Size(292, 39);
             labTituto.TabIndex = 0;
-            labTituto.Text = "Gestión general";
+            labTituto.Text = "Gestión general  ";
+            labTituto.Click += labTituto_Click;
             // 
             // panelVisualCitas
             // 
