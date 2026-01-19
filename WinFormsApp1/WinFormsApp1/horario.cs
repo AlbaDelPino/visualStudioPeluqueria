e72055aff48527920c3c195c9fdd0a8ae63fe509
Alba Del Pino Ciges <albadelpino204@gmail.com>
2026-01-19 10:17:30 +0100

    horario
    d

---
+++ WinFormsApp1/WinFormsApp1/horario.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class horario : Form
+    {
+
+        private readonly string _token;
+        public horario(string token)
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.ResizeRedraw = true;
+
+            _token = token;
+        }
+
+        private void horario_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+    }
+}
